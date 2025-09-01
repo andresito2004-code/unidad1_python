aa8000f78e3e662d13f5185dc97bb3a83f08efd2
Laboratorio Computación N° 8 <claseslab8@inacap.cl>
2025-09-01 16:45:46 -0400

    clase admin

--- monitoreo/dispositivos/admin.py
+++ monitoreo/dispositivos/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Dispositivos
+
+@admin.register(Dispositivos)
+class DispositivosAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'consumo_maximo', 'estado')
+    list_filter = ('consumo_maximo', 'estado')
+    search_fields = ('nombre',)
+
+
+
